45280ffded3c5c74c677355f900d8334572ca1f7
wb_lihiayang@meituan.com <18181378011@163.com>
2022-08-01 08:29:18 +0800

    添加项目文件。

---
+++ yx_class/reverseSqueue.cpp
@@ -0,0 +1,15 @@
+///*
+//	实现一个空间复杂度为O(1)的顺序表逆置算法。
+//	分析：从第一个元素开始遍历到中间元素，使用中间变量对a[i]与a[n-i+1]进行调换，即可完成
+//*/
+//#include <stdio.h>
+//void revert(int *arr,int len) {
+//	
+//}
+//int main() {
+//	int arr[] = {1,2,3,4,5,6,7};//这里的数组也可以手动赋值
+//	int len = sizeof(arr)/4;
+//	revert(arr,len); 
+//	for (int i = 0; i < len;i++) printf("%d ",*(arr+i));
+//	return 0;
+//}
